ce4e0410b5baeb4604231e0b2c2d4005a882e6a1
Miro Manestar <miroimanestar@gmail.com>
2021-03-24 23:28:39 -0400

    Added more sliders and lazy loading

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
+    loadMap();
+    loadSwiper();
     hideLoader();
 });
+
+window.onresize = loadMap;
 
 function hideLoader() {
     setTimeout(function() {
@@ -8,34 +12,64 @@ function hideLoader() {
     }, 2000);
 }
 
-var world_map = L.map('world-map', {
-    zoomSnap: 0.25,
-    zoomControl: false,
-    scrollWheelZoom: false,
-    maxBoundsViscosity: 0.50,
-    worldCopyJump: true
-}).setView([40, 0], 1.50);
+var world_map;
+function loadMap() {
+    if (world_map != undefined) {
+        world_map.remove();
+        $('#world-map').replaceWith('<div id="world-map" class="map swiper-slide"></div>')
+    }
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox/light-v10',
-    maxZoom: 3,
-    minZoom: 1,
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'sk.eyJ1IjoibWlyb21hbmVzdGFyIiwiYSI6ImNrbDFzYTAwejEwbnUyd3VpaHV1eWZzZmoifQ.yKU91MtkBHK1Cd_TfGkr_Q',
-}).addTo(world_map);
+    world_map = L.map('world-map', {
+        zoomSnap: 0.25,
+        zoomControl: false,
+        scrollWheelZoom: false,
+        maxBoundsViscosity: 0.50,
+        worldCopyJump: true,
+        dragging: false,
+    }).setView([40, 0], 1.50);
+    
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox/light-v10',
+        maxZoom: 3,
+        minZoom: 1,
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'sk.eyJ1IjoibWlyb21hbmVzdGFyIiwiYSI6ImNrbDFzYTAwejEwbnUyd3VpaHV1eWZzZmoifQ.yKU91MtkBHK1Cd_TfGkr_Q',
+    }).addTo(world_map);
+    
+    /* Add to tileLayer to remove duplicate map
+        noWrap: true,
+        bounds: [
+            [-90, -180],
+            [90, 180]
+        ]
+    */
 
-/* Add to tileLayer to remove duplicate map
-    noWrap: true,
-    bounds: [
-        [-90, -180],
-        [90, 180]
-    ]
-*/
+        
+    world_map.fitWorld().zoomIn();
+    world_map.setMaxBounds(world_map.getBounds());
+        console.log(world_map.getBounds())
+    $('.leaflet-control-attribution.leaflet-control').remove();
+}
 
+function loadSwiper() {
+    //Initialize swiper
+    const showcaseSwiper = new Swiper('.swiper-container', {
+        lazy: true,
+        spaceBetween: 1,
+        loop: true,
+        centeredSlides: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+    });
 
-world_map.fitWorld().zoomIn();
-world_map.setMaxBounds(world_map.getBounds());
+    const exploreSwiper = new Swiper('.explore-container', {
+        direction: 'vertical',
+        spaceBetween: 1,
+        mousewheel: true
+    });
+}
 
-$('.leaflet-control-attribution.leaflet-control').remove();
